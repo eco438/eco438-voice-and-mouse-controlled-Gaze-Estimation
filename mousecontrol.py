e4c7328fb7def26e1052e0d99892c8c68e6f37ee
Emmanuel Okafor <oka4emmanuel97@gmail.com>
2021-02-28 19:15:15 -0500

    added clicking functionality

--- mousecontrol.py
+++ mousecontrol.py
@@ -1,27 +1,47 @@
 import speech_recognition
 import pyautogui
-
+from word2number import w2n
 recognizer = speech_recognition.Recognizer()
 while True:
-    with speech_recognition.Microphone() as src:
+    with speech_recognition.Microphone(device_index=2) as src:
         try: 
+            
             audio  = recognizer.adjust_for_ambient_noise(src)
             print("Threshold Value After calibration:" + str(recognizer.energy_threshold))
             print("Please speak")
             audio = recognizer.listen(src)
             speech_to_txt = recognizer.recognize_google(audio).lower()
-            if (speech_to_txt == "quit program") or (speech_to_txt == "exit program"):
-                break
-            elif speech_to_txt == "mouse up" or speech_to_txt == "move up":
-                pyautogui.moveRel(0,-100)
-            elif speech_to_txt == "mouse down" or speech_to_txt == "move down":
-                pyautogui.moveRel(0,100)
-            elif speech_to_txt == "mouse left" or speech_to_txt == "move left":
-                pyautogui.moveRel(-100,0)
-            elif speech_to_txt == "mouse right" or speech_to_txt == "move right":
-                pyautogui.moveRel(100,0)
+            words = speech_to_txt.split(" ")
+            if len(words) <2:
+                if (speech_to_txt == "quit") or (speech_to_txt == "exit"):
+                    break
+                elif speech_to_txt == "up":
+                    pyautogui.moveRel(0,-100)
+                elif speech_to_txt == "down":
+                    pyautogui.moveRel(0,100)
+                elif speech_to_txt == "left":
+                    pyautogui.moveRel(-100,0)
+                elif speech_to_txt == "right":
+                    pyautogui.moveRel(100,0)
+                elif speech_to_txt == "click":
+                    pyautogui.doubleclick()
+                else:
+                    print("Wrong Command")
             else:
-                print("Wrong Command")
+                if "quit" in speech_to_txt:
+                    break
+                elif "up" in speech_to_txt:
+                    pyautogui.moveRel(0,-10*int(words[1]))
+                elif "down" in speech_to_txt:
+                    pyautogui.moveRel(0,10*int(words[1]))
+                elif "left" in speech_to_txt:
+                    pyautogui.moveRel(-10*int(words[1]),0)
+                elif "write" in speech_to_txt:
+                    pyautogui.moveRel(10*int(words[1]),0)
+                elif speech_to_txt == "click":
+                    pyautogui.doubleclick()
+                else:
+                    print("Wrong Command")
         except Exception as ex:
             print("sorry. Could not understand.")
         
