642247270d7c9b1b9e7e6a2c4f845441d67f6d8a
Emmanuel Okafor <oka4emmanuel97@gmail.com>
2021-03-15 11:02:05 -0400

    Updated the models

--- mousecontrol.py
+++ mousecontrol.py
@@ -1,29 +1,50 @@
+
 import speech_recognition
 import pyautogui
 from word2number import w2n
 recognizer = speech_recognition.Recognizer()
+
+from ibm_watson import SpeechToTextV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
+from ibm_watson import ApiException
+from ibm_watson.websocket import RecognizeCallback, AudioSource
+import json
+
+authenticator = IAMAuthenticator('B88jZ6CWcaCfY-gES0dPk6xxQA2E1Ufg46BwOc5GpKYE')
+speech_to_text = SpeechToTextV1(
+    authenticator=authenticator
+)
+
+speech_to_text.set_service_url('https://api.us-east.speech-to-text.watson.cloud.ibm.com/instances/097def43-7968-4858-82d9-6a817455f100')
 while True:
-    with speech_recognition.Microphone(device_index=2) as src:
-        try: 
-            
-            audio  = recognizer.adjust_for_ambient_noise(src)
-            print("Threshold Value After calibration:" + str(recognizer.energy_threshold))
-            print("Please speak")
-            audio = recognizer.listen(src)
-            speech_to_txt = recognizer.recognize_google(audio).lower()
-            words = speech_to_txt.split(" ")
+    with speech_recognition.Microphone() as src:
+        recognizer.adjust_for_ambient_noise(src)
+        print("Threshold Value After calibration:" + str(recognizer.energy_threshold))
+        print("Please speak")
+        audio = recognizer.listen(src,phrase_time_limit=1)
+
+    try:
+        speech = speech_to_text.recognize(audio=audio.get_wav_data(),content_type='audio/wav',keywords=['up','down','left','right'],keywords_threshold=0.8,inactivity_timeout = 3,smartFormatting=True,endOfPhraseSilenceTime=0.1,customization_weight = 0.9,language_customization_id= "79224d58-3bbc-4444-ab7d-0943153645df",base_model_name= "en-US_BroadbandModel").get_result()
+        print(json.dumps(speech, indent=2))
+        print(speech)
+
+    except ApiException as ex:
+        print ("Method failed with status code " + str(ex.code) + ": " + ex.message)
+  
+    """
+    try:
             if len(words) <2:
                 if (speech_to_txt == "quit") or (speech_to_txt == "exit"):
                     break
-                elif speech_to_txt == "up":
+                elif "up" in speech_to_txt:
                     pyautogui.moveRel(0,-100)
-                elif speech_to_txt == "down":
+                elif "down" in speech_to_txt:
                     pyautogui.moveRel(0,100)
-                elif speech_to_txt == "left":
+                elif "left" in speech_to_txt:
                     pyautogui.moveRel(-100,0)
                 elif speech_to_txt == "right":
                     pyautogui.moveRel(100,0)
-                elif speech_to_txt == "click":
+                elif "select" in speech_to_txt:
                     pyautogui.doubleclick()
                 else:
                     print("Wrong Command")
@@ -42,7 +63,8 @@ while True:
                     pyautogui.doubleclick()
                 else:
                     print("Wrong Command")
-        except Exception as ex:
-            print("sorry. Could not understand.")
+    except Exception as ex:
+        print("sorry. Could not understand.")
+    """
         
         
