14f1fb42910ea350ca02869cfee6ce9890095148
Emmanuel Okafor <oka4emmanuel97@gmail.com>
2021-04-11 19:16:55 -0400

    fixed screen issue

--- Point_generator.py
+++ Point_generator.py
@@ -1,36 +1,37 @@
 import turtle
 import random
 import keyboard
+from screeninfo import get_monitors
 
 def nine_point_generator():
-    lst = [(0,400),(0,-400),(-600,-400),(600,400),(600,0),(-600,0),(-600,400),(600,-400),(0,0)]
-    for i in lst:
-        turtle.up()
-        turtle.goto(i[0],i[1])
-        turtle.down()
-        turtle.dot(50)
-        print(turtle.pos())
+    value_x = get_monitors()[0].width//4
+    value_y = get_monitors()[0].height//4
+    x = value_x
+    y = value_y
+    lst = []
+    for x in range(value_x,get_monitors()[0].width,value_x):
+        for y in range(value_y,get_monitors()[0].height,value_y):
+            lst.append((x,y))
+    return lst
 def thirty_point_generator():
-   
+    lst =[]
     count = 0
     while count <30:
-        x = random.randrange(600)
-        y = random.randrange(400)
-        ans = None
-        turtle.up()
-        turtle.goto(x,y)
-        turtle.down()
-        turtle.dot(30)
-        keyboard.wait("space")
+        x = random.randrange(get_monitors()[0].width-100)
+        y = random.randrange(get_monitors()[0].height-100)
        
-        turtle.clear()
+        lst.append((x,y))
+        count+=1
+    return lst
         
         
 
 if __name__ == "__main__":
     random.seed()
     sc= turtle.Screen()
-    sc.setup(1.0,1.0)
+    turtle.setup(1.0,1.0)
+    sc.mode('World')
+    turtle.setworldcoordinates(0,get_monitors()[0].height,get_monitors()[0].width,0)
     turtle.speed(0)
-    nine_point_generator()
+    print(thirty_point_generator())
     turtle.done()
